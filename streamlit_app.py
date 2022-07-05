bd6e1ccab105909b470c61e097607c4ce05826d4
selvapraveene <108715582+selvapraveene@users.noreply.github.com>
2022-07-05 12:38:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Dinner')
 
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('emoji.demojize(":rice:") Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale ,Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
