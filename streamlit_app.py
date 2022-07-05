0551b0b8965598e68ec2725c2ee87f2561834a77
selvapraveene <108715582+selvapraveene@users.noreply.github.com>
2022-07-05 12:48:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import emoji
 
 streamlit.title('My Parents New Healthy Dinner')
-print(":bowtie:")
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale ,Spinach & Rocket Smoothie')
