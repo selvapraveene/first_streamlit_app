8e66b16357849ab09dc0bb9042af5897895318a2
selvapraveene <108715582+selvapraveene@users.noreply.github.com>
2022-07-05 12:52:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-print("\UOOOO1f6OO")
+
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale ,Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+Streamlit.header('Build your own Fruit Smoothie')
