ca210f943f10563f90454e2c052ca2349116b44e
selvapraveene <108715582+selvapraveene@users.noreply.github.com>
2022-07-05 12:40:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-print(":bowtie:")
+print("\U0001f600")
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale ,Spinach & Rocket Smoothie')
