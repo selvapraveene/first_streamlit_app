5bcb8eaba5112ba23aa62a24ca374fa5282cda33
selvapraveene <108715582+selvapraveene@users.noreply.github.com>
2022-07-06 15:03:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,17 +60,25 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
 #streamlit.header("Fruityvice Fruit Advice!")
   streamlit.dataframe(my_data_rows)
+#Allow users to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values('" + new_fruit +"')")
+    return "Thanks for adding " + new_fruit
 
+  
 add_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('The user entered ', add_fruit)
 
-streamlit.stop()
+#streamlit.stop()
 
 add_fruit_response = requests.get("https://fruityvice.com/api/fruit/"+add_fruit)
 #streamlit.text(add_fruit_response.json()) # writes data to the screen
-my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('" +add_fruit + "')")
+#my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('" +add_fruit + "')")
+insert_row_snowflake(add_fruit)
 
 fruit_normalized = pandas.json_normalize(add_fruit_response.json())
 # draws a table for the results
